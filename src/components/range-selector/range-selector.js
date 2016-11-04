357fe6800c30dccd451df7ad193b429b4149818f
Ivan Kotov <kotovivan@gmail.com>
2016-11-04 14:25:10 +0300

    Initial

---
+++ src/components/range-selector/range-selector.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RangeSelector extends Component {
+  render() {
+    return (
+      <div className="range-selector">
+      implement RangeSelector
+      </div>
+    );
+  }
+}
+
+export default RangeSelector;
