dc79c38cd39ea88c2441ddda3857a39f50df903b
Ivan Kotov <kotovivan@gmail.com>
2016-11-04 16:05:15 +0300

    some styling

--- src/components/range-selector/range-selector.js
+++ src/components/range-selector/range-selector.js
@@ -2,6 +2,10 @@ import { connect } from 'react-redux'
 import { updateAmount } from '../../actions'
 import React, { PropTypes } from 'react';
 import _ from 'lodash';
+import block from 'bem-cn';
+
+import './range-selector.css';
+const b_ = block('range-selector');
 
 const DFLT_VALUE = 1e6;
 
@@ -28,11 +32,13 @@ class RangeComponent extends React.Component {
   }
 
   render() {
+    const inputValue = this.props.value + '';
     const inputProps = _.pick(this.props, ['min', 'max', 'step', 'value']);
 
     return (
-      <div className='range-selector'>
-        <div>{ this.props.value }</div>
+      <div className={ b_() }>
+        <div className={ b_('main-part') }>{ inputValue.substr(0, inputValue.length - 2) }</div>
+        <div className={ b_('cents-part') }>{ inputValue.substr(-2) }</div>
         <input type='range' { ...inputProps } onChange={ this.handleChange } />
       </div>
     );
